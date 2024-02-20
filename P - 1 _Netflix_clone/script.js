5471c753f676f5ba74104b741c7da8330ac38225
Vatsal-web-design <160050505+Vatsal-web-design@users.noreply.github.com>
2024-02-20 22:05:06 +0000

    Add files via upload

---
+++ P - 1 _Netflix_clone/script.js
@@ -0,0 +1,49 @@
+let c1 = document.querySelector(".c1")
+let d1 = document.querySelector(".d1")
+let d2 = document.querySelector(".d2")
+let d3 = document.querySelector(".d3")
+let d4 = document.querySelector(".d4")
+let d5 = document.querySelector(".d5")
+let d6 = document.querySelector(".d6")
+let rorate = document.getElementById("rorate")
+
+// let c = c1.addEventListener("click", (e) => {
+//     let pera = document.createElement("p");
+//     pera.className = "a1";
+//     pera.innerHTML = "Netflix is a streaming service that offers a wide variety of award-winning TV programmes, films, anime, documentaries and more on thousands of internet-connected devices."
+//     q1.appendChild(pera)
+//     // pera.classList.toggle("a1")
+    
+// }
+// )
+// del.addEventListener("click",(e)=>{
+//     d1.remove()
+// })
+
+function myfunction(){
+    rorate.classList.toggle("del")
+    d1.classList.toggle("hide")
+
+    // d1.toggle("b")
+}
+function myfunction2(){
+    d2.classList.toggle("hide")
+    // d1.toggle("b")
+}
+function myfunction3(){
+    d3.classList.toggle("hide")
+    // d1.toggle("b")
+}
+function myfunction4(){
+    d4.classList.toggle("hide")
+    // d1.toggle("b")
+}
+function myfunction5(){
+    d5.classList.toggle("hide")
+    // d1.toggle("b")
+}
+function myfunction6(){
+    d6.classList.toggle("hide")
+    // d1.toggle("b")
+}
+
